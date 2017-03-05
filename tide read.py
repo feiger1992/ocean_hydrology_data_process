851c29696c66023d7634f7ef709e3908de4f63d4
feiger1992 <983232781@qq.com>
2017-03-05 18:24:30 +0800

    add 2 small modules
    add the modules of split by time, analysis the local extreme height

--- tide read.py
+++ tide read.py
@@ -15,7 +15,7 @@ def process(y):
     global switch_of_process
     switch_of_process = True
     rft=np.fft.rfft(y)
-    rft[int(len(rft)/2):] = 0
+    rft[int(len(rft)/4):] = 0
     return np.fft.irfft(rft)
 
 class Tide (object):
@@ -36,11 +36,45 @@ class Tide (object):
         temp_data['format_time'] = t
         temp_data['tide_init'] = temp_data.tide
         temp_data.tide = process(temp_data.tide)
+        temp_data.index = temp_data['format_time']
 
         temp_data = temp_data.ix[temp_data.format_time <= time2]
         temp_data = temp_data.ix[temp_data.format_time >= time1]
 
+        temp_data['if_min'] = np.r_[True, temp_data.tide.values[1:] < temp_data.tide.values[:-1]] & np.r_[
+            temp_data.tide.values[:-1] < temp_data.tide.values[1:], True]
+        temp_data['if_max'] = np.r_[True, temp_data.tide.values[1:] > temp_data.tide.values[:-1]] & np.r_[
+            temp_data.tide.values[:-1] > temp_data.tide.values[1:], True]
+        temp_data.set_value(temp_data.index.max(), 'if_min', False)
+        temp_data.set_value(temp_data.index.max(), 'if_max', False)
+        temp_data.set_value(temp_data.index.min(), 'if_max', False)
+        temp_data.set_value(temp_data.index.min(), 'if_min', False)
 
+        temp_data['raising_time'] = datetime.timedelta(0)
+        temp_data['ebb_time'] = datetime.timedelta(0)
+        for x in temp_data.ix[temp_data.if_max == True].format_time:
+            try:
+                temp_data.loc[x, 'raising_time'] = x - temp_data[
+                    (temp_data.if_min == True) & (temp_data.format_time < x)].format_time.max()
+            except:
+                pass
+
+        for x in temp_data.ix[temp_data.if_min == True].format_time:
+            try:
+                temp_data.loc[x, 'ebb_time'] = x - temp_data[
+                    (temp_data.if_max == True) & (temp_data.format_time < x)].format_time.max()
+            except:
+                pass
+        self.year_tide = []
+        self.month_tide = []
+        self.day_tide = []
+        for i, j in temp_data.groupby(temp_data.index.year):  #
+            self.year_tide.append(j)
+            for ii, jj in j.groupby(j.index.month):
+                self.month_tide.append(jj)
+                for iii, jjj in jj.groupby(jj.index.day):
+                    self.day_tide.append(jjj)
+        self.data = temp_data
     def plot_tide_compare(self):
         self.fig0, self.ax0 = plt.subplots()
         fig = self.fig0
@@ -57,4 +91,5 @@ class Tide (object):
 if __name__ == '__main__':
     name = "test.xlsx"
     x =Tide(name)
-    x.plot_tide_compare()
+    y =x.data
+    print(y)
